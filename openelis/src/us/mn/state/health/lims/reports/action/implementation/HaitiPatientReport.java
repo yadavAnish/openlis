e6a53efc9c59efdfad86717842ee12b78b4ce63e
Anish yadav <email.anishy@gmail.com>
2023-06-05 10:34:38 +0545

    f

--- openelis/src/us/mn/state/health/lims/reports/action/implementation/HaitiPatientReport.java
+++ openelis/src/us/mn/state/health/lims/reports/action/implementation/HaitiPatientReport.java
@@ -759,6 +759,7 @@ public abstract class HaitiPatientReport extends Report {
         HaitiClinicalPatientData data = new HaitiClinicalPatientData();
         String testName = null;
         String sortOrder = "";
+        String loginNamee=getLoginName();
 
         boolean doAnalysis = reportAnalysis != null;
 
@@ -782,7 +783,7 @@ public abstract class HaitiPatientReport extends Report {
         data.setHealthRegion(getLazyPatientIdentity(healthRegion, HEALTH_REGION_IDENTITY_TYPE_ID));
         data.setHealthDistrict(getLazyPatientIdentity(healthDistrict, HEALTH_DISTRICT_IDENTITY_TYPE_ID));
         data.setTestName(testName);
-        data.setLoginName(getLoginName());
+        data.setLoginName(loginNamee());
         if (currentProvider != null) {
             data.setPatientSiteNumber(currentProvider.getExternalId());
         }
